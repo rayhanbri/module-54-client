d5edaf5ddba514ab6f7688f49f36ed6202876197
rayhanbri <mohammadrayhanx0@gmail.com>
2025-05-15 13:50:15 -0700

    getting data

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,16 +1,49 @@
 import React, { use } from 'react';
 
-const Users = ({userPromise}) => {
+const Users = ({ userPromise }) => {
     const users = use(userPromise);
-    console.log(users)
+    // console.log(users)
+
+const handleUser = e => {
+    e.preventDefault();
+    
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const user = {name, email};
+    console.log(user)
+
+    // crate user in the sever 
+
+    fetch('http://localhost:3000/users')
+    .then(res => res.json() )
+    .then (data => {
+        console.log( 'data after post ',data)
+    })
+
+}
+
+
+
+
     return (
         <div>
-            {
-                users.map(user => <p key={user.id}>
-                     {user.name} : {user.email}
 
-                </p>)
-            }
+            <form onSubmit={handleUser}>
+                <input name="name" type="text"  id="" />
+                <br />
+                <input name='email' type="email"  id="" />
+                <br />
+                <input type="submit" value="Submit" />
+
+            </form>
+            <div>
+                {
+                    users.map(user => <p key={user.id}>
+                        {user.name} : {user.email}
+
+                    </p>)
+                }
+            </div>
         </div>
     );
 };
